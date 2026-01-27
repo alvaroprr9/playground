9f3473f2c7a28ff2bd3597a1ce958cb984ad31a4
Ismael Bueno <ismaelwno@gmail.com>
2026-01-27 20:50:41 +0100

    Conflict example

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
-int main() {
+int main()
+{
+    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Hello, world!" << std::endl;
     std::cout << "Hello, world!" << std::endl;
     return 0;
 }
-
