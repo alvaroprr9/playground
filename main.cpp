b43c8a66513ed2eccd97aab4bf19249f0a33f831
Ismael Bueno <ismaelwno@gmail.com>
2026-02-04 19:31:08 +0100

    chore: Minor changes

--- main.cpp
+++ main.cpp
@@ -1,52 +1,15 @@
-#include <iostream>
-using namespace std;
-
-void helloWorld()
-{
-    cout << "HelloWorld" << endl;
-}
-
-void HelloWorldIf(int x)
-{
-    if (x == 3)
-        helloWorld();
-}
-
-void HelloWorldMany(int n)
-{
-    for (int i = 0; i < n; i++)
-        helloWorld();
-}
-
-void HelloWorldWhile()
-{
-    int i = 0;
-    while (i < 4)
-    {
-        cout << "HelloWorld" << endl;
-        i++;
-    }
-}
-
-void imprimirArray(int *arr, int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d\n", arr[i]);
-    }
-}
+#include <stdio.h>
 
 void sumArrays(int *a, int *b, int *c, int n)
 {
     for (int i = 0; i < n; i++)
     {
-        *(c + i) = *(a + i) + *(b + i);
+        c[i] = a[i] + b[i];
     }
 }
 
 int main()
 {
-
     int A[] = {1, 2, 3, 4};
     int B[] = {1, 2, 3, 4};
     constexpr int n = 4;
@@ -54,38 +17,7 @@ int main()
     sumArrays(A, B, C, n);
     for (int i = 0; i < 4; i++)
     {
-        printf("%d ", *(C + i));
+        printf("%d ", C[i]);
     }
     return 0;
 }
-// int main()
-// {
-//     HelloWorldIf(3);
-//     HelloWorldIf(2);
-//     HelloWorldMany(4);
-//     int n;
-//     int n2 = 3;
-//     int *ptr = &n;
-//
-//     cout << "cuantas veces?";
-//     cin >> n;
-//     HelloWorldMany(n);
-//     int size;
-//     cout << "Cuantos numeros quieres generar?";
-//     cin >> size;
-//     vector<int> numeros(size);
-//
-//     for (int i = 0; i < size; i++)
-//     {
-//         cout << "Ingresa el numero" << i + 1 << ":";
-//         cin >> numeros[i];
-//     }
-//
-//     cout << " Los numeros ingresados son: ";
-//     for (int i = 0; i < size; i++)
-//     {
-//         cout << numeros[i] << " ";
-//     }
-//     cout << endl;
-//     return 0;
-// }
