c4744569243ba0ad90869415f3f01123cf642512
Ismael Bueno <ismaelwno@gmail.com>
2026-02-12 22:12:30 +0100

    feat: Start generic stack

--- main.cpp
+++ main.cpp
@@ -1,42 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
+#include <memory>
 
 typedef int32_t i32;
 typedef uint32_t u32;
 
+// 1 byte -> 8bits
+
 typedef struct
 {
-    i32 *data;
+    std::byte *data;
     u32 capacity;
     u32 size;
-    u32 initial_capacity;
 } Stack;
 
 Stack *stack_create(u32 initial_capacity)
 {
     Stack *stack = (Stack *)malloc(sizeof(Stack));
-    stack->data = (i32 *)malloc(initial_capacity * sizeof(i32));
+    stack->data = (std::byte *)malloc(initial_capacity);
     stack->capacity = initial_capacity;
     stack->size = 0;
-    stack->initial_capacity = initial_capacity;
     return stack;
 }
 
-void stack_push(Stack *stack, i32 value)
+// + + + + - - - - - - - -
+// >
+// memory -> + + + + + + | |
+//
+// size = 4
+// psize = 6
+//
+// dst = data + size
+// src = memory
+// s = psize
+//
+// memcpy(dst, src, s)
+//
+// data = > +
+//
+//
+// size = 4
+//
+// memory, 4
+//
+//
+//
+
+void stack_push(Stack *stack, std::byte *memory, u32 size)
 {
-    if (stack->size == stack->capacity)
+    if (stack->size + size > stack->capacity)
     {
         u32 new_capacity = stack->capacity * 2;
-        i32 *ndata = (i32 *)malloc(new_capacity * sizeof(i32));
+        std::byte *ndata = (std::byte *)malloc(new_capacity);
         if (!ndata)
         {
             printf("Error redimensionando el stack\n");
             return;
         }
 
-        for (u32 i = 0; i < stack->size; ++i)
-            ndata[i] = stack->data[i];
+        memcpy(ndata, stack->data, stack->size);
 
         free(stack->data);
         stack->data = ndata;
@@ -45,46 +69,22 @@ void stack_push(Stack *stack, i32 value)
         printf("Capacidad aumentada a %u\n", new_capacity);
     }
 
-    stack->data[stack->size] = value;
-    stack->size++;
+    std::byte *dst = stack->data + stack->size;
+    std::byte *src = memory;
+
+    memcpy(dst, src, size);
+
+    stack->size += size;
+
+    // stack->data[stack->size] = value;
+    // stack->size++;
 }
 
 i32 stack_pop(Stack *stack)
 {
-    if (stack->size == 0)
-    {
-        printf("Error: stack vacio\n");
-        return 0
-    }
-
-    stack->size--;
-    i32 value = stack->data[stack->size];
-
-    if (stack->size <= stack->capacity / 4 && stack->capacity / 2 >= stack->initial_capacity)
-    {
-        u32 new_capacity = stack->capacity / 2;
-        i32 *ndata = (i32 *)malloc(new_capacity * sizeof(i32));
-        if (!ndata)
-        {
-            printf("Error redimensionando el stack\n");
-            return value;
-        }
-
-        for (u32 i = 0; i < stack->size; ++i)
-            ndata[i] = stack->data[i];
-
-        free(stack->data);
-        stack->data = ndata;
-        stack->capacity = new_capacity;
-
-        printf("Capacidad reducida a %u\n", new_capacity);
-    }
-
-    return value;
 }
 
-
-void stack_free(Stack * stack)
+void stack_free(Stack *stack)
 {
     if (stack)
     {
@@ -100,7 +100,7 @@ int main()
     if (!stack)
         return 1;
 
-    stack_push(stack, 10);
+    // stack_push(stack, 10);
 
     printf("Pop: %d\n", stack_pop(stack));
     stack_free(stack);
