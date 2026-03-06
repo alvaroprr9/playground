8b01d7a68020dbbcb4a9972abc134c9f2e02d3bd
Ismael Bueno <ismaelwno@gmail.com>
2026-03-06 21:27:24 +0100

    feat: Draw some stuff

--- main.cpp
+++ main.cpp
@@ -1,24 +1,79 @@
 #include "onyx/core/core.hpp"
 #include "onyx/platform/window.hpp"
+#include "onyx/asset/assets.hpp"
 #include "onyx/execution/execution.hpp"
+#include "onyx/rendering/context.hpp"
 
 int main()
 {
+    using Onyx::D2;
+    using Onyx::D3;
+    using namespace TKit::Alias;
+
+    // --------- CPU
+    // |
+    // v
+    // _________ GPU -> CPU VISIBLE
+    // |
+    // v
+    // +++++++++ GPU -> DEVICE LOCAL
+
     ONYX_CHECK_EXPRESSION(Onyx::Initialize());
+    Onyx::RenderContext<D2> *ctx = ONYX_CHECK_EXPRESSION(Onyx::Renderer::CreateContext<D2>());
+
+    const Onyx::StatMeshData<D2> data = Onyx::Assets::CreateSquareMesh<D2>();
+    const Onyx::Mesh square = Onyx::Assets::AddMesh(data);
+    ONYX_CHECK_EXPRESSION(Onyx::Assets::Upload());
 
     Onyx::Window *window = ONYX_CHECK_EXPRESSION(Onyx::Platform::CreateWindow());
+    window->CreateCamera<D2>();
+    ctx->AddTarget(window);
+    Onyx::PointLight<D2> *pl = ctx->AddPointLight();
+
+    f32 time = 0.f;
     while (!window->ShouldClose())
         if (ONYX_CHECK_EXPRESSION(window->AcquireNextImage(Onyx::Block)))
         {
+            ctx->Flush();
+            const f32 x = cosf(time);
+
+            ctx->FillColor(Onyx::Color::Orange);
+            ctx->Push();
+            ctx->Scale(0.5f);
+            ctx->TranslateX(x);
+            pl->SetPosition(f32v2{x, 0.f});
+            ctx->StaticMesh(square);
+            ctx->Pop();
+
+            // ctx->Scale(2.f);
+            ctx->Circle();
+            time += 0.001f;
+
             Onyx::Input::PollEvents();
+
+            VKit::Queue *tqueue = Onyx::Execution::FindSuitableQueue(VKit::Queue_Transfer);
             VKit::Queue *gqueue = Onyx::Execution::FindSuitableQueue(VKit::Queue_Graphics);
+
+            ONYX_CHECK_EXPRESSION(tqueue->UpdateCompletedTimeline());
             ONYX_CHECK_EXPRESSION(gqueue->UpdateCompletedTimeline());
+
+            Onyx::CommandPool *tpool =
+                ONYX_CHECK_EXPRESSION(Onyx::Execution::FindSuitableCommandPool(VKit::Queue_Transfer));
             Onyx::CommandPool *gpool =
                 ONYX_CHECK_EXPRESSION(Onyx::Execution::FindSuitableCommandPool(VKit::Queue_Graphics));
 
+            const VkCommandBuffer tcmd = ONYX_CHECK_EXPRESSION(Onyx::Execution::Allocate(tpool));
+
+            ONYX_CHECK_EXPRESSION(Onyx::Execution::BeginCommandBuffer(tcmd));
+            const Onyx::Renderer::TransferSubmitInfo tsinfo =
+                ONYX_CHECK_EXPRESSION(Onyx::Renderer::Transfer(tqueue, tcmd));
+            ONYX_CHECK_EXPRESSION(Onyx::Execution::EndCommandBuffer(tcmd));
+            if (tsinfo)
+                ONYX_CHECK_EXPRESSION(Onyx::Renderer::SubmitTransfer(tqueue, tpool, tsinfo));
+
             const VkCommandBuffer gcmd = ONYX_CHECK_EXPRESSION(Onyx::Execution::Allocate(gpool));
             ONYX_CHECK_EXPRESSION(Onyx::Execution::BeginCommandBuffer(gcmd));
-            // Onyx::Renderer::ApplyAcquireBarriers(gcmd);
+            Onyx::Renderer::ApplyAcquireBarriers(gcmd);
 
             window->BeginRendering(gcmd);
             const Onyx::Renderer::RenderSubmitInfo rinfo =
